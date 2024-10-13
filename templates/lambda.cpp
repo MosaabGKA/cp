e11c112fa9b4f5035e9e4ae9b4b3990fe07cb896
Mosaab Gamal Kamel Abdelaleem <mosaabgka@outlook.com>
2024-10-13 16:20:11 +0300

    add another lambda

--- templates/lambda.cpp
+++ templates/lambda.cpp
@@ -6,3 +6,11 @@ function<void(ll)> remove = [&](ll i) {
         remove(j);
     }
 };
+
+auto getY = [&](int from, int to, int y) -> pair<int , int> {
+    int left = y - abs(from - to);
+    int right = y + abs(from - to);
+    left = max(left, 1);
+    right = min(right, m);
+    return make_pair(left, right);
+};
